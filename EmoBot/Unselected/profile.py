27607c4953b606a11217fa7c9a1bb065793a9176
nsbarnett <115379833+nsbarnett@users.noreply.github.com>
2025-04-15 20:10:40 -0500

    Created 'Selected' and 'Unselected' folders

---
+++ EmoBot/Unselected/profile.py
@@ -0,0 +1,14 @@
+class Profile:
+    def __init__(self, name: str):
+        self.name = name
+        self.chat_history = []
+        self.pre_chat_mood = None
+        self.post_chat_mood = None
+
+    def to_dict(self):
+        return {
+            'name': self.name,
+            'chat_history': self.chat_history,
+            'pre_chat_mood': self.pre_chat_mood,
+            'post_chat_mood': self.post_chat_mood
+        }
